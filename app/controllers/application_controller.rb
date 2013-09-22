ff182776a89c46f116b884cdb9c00488b7925949
Erkki Heino <evsheino@gmail.com>
2013-09-22 21:55:59 +0300

    Refactoring, fixed root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  include SessionsHelper
   before_filter :authenticate
 
   def authenticate
